ba6a7379681dbdb8217ab9a892109a7a2acdfca8
cindy <z18603256112@outlook.com>
2025-09-08 08:41:55 +0800

    加了1张图

--- js/config.js
+++ js/config.js
@@ -1,12 +1,12 @@
 // --- 游戏全局配置文件 ---
 
 // 玩家相关配置
-export const PLAYER_WIDTH = 30;
-export const PLAYER_HEIGHT = 50;
+export const PLAYER_WIDTH = 100;
+export const PLAYER_HEIGHT = 100;
 export const PLAYER_SPEED = 2; // 玩家的移动速度
 
 // 交互相关配置
-export const INTERACTION_RADIUS = 80; // 可交互对象的检测半径
+export const INTERACTION_RADIUS = 150; // 可交互对象的检测半径
 export const BASE_VISUAL_GAP = 15; // 提示标签和可交互对象的间距
 
 // 小游戏相关配置
