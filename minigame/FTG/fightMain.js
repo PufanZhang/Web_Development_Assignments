fc79bba3706766b70a0feb22c53b009faafff5b4
zhq-aska <tokitori9961@gmail.com>
2025-09-12 02:00:30 +0800

    STG、跑酷、FTG、马里奥小游戏更新兼容 但FTG小游戏好像有点问题

--- minigame/FTG/fightMain.js
+++ minigame/FTG/fightMain.js
@@ -1,10 +1,11 @@
+import { getCurrentUser } from "../../js/game/modules/dataManager.js"
 // import { player } from "../../js/modules/world/player.js"
 import { fightManager } from "./fight.js"
 import { minigameLoader } from "../../js/game/modules/minigameLoader.js";
 
 // --- 全局游戏状态 ---
 // window.gameMode = "map" // 'map' 或 'dialogue'
-//let currentUser = null
+let currentUser = null
 // --- 当前地图的状态容器 ---
 // let currentMap = {
 //   id: null,
@@ -13,6 +14,13 @@ import { minigameLoader } from "../../js/game/modules/minigameLoader.js";
 // }
 // --- 游戏初始化 ---
 async function initializeGame() {
+    currentUser = getCurrentUser()
+    if (!currentUser) {
+        alert("请先登录！")
+        window.location.href = "/login.html"
+        return
+    }
+    // player.init()
     fightManager.start()
     const startButton = document.getElementById("start-game")
     const backButton = document.getElementById("back-game")
